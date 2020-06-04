4a1853d9a89801fe4282c42ff87b48c88e2fbb9f
Yannick Lagger <lagger.yannick@gmail.com>
2020-06-04 23:03:42 +0200

    Refactor get flights and add flight filter

--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -12,34 +12,54 @@ export class FlightService {
     ) { }
 
     async getFLights(token: any, query: any): Promise<Flight[]> {
-        let options: any = {
-            relations: ["glider", "start", "landing"],
-            where: {
-                user: {
-                    id: token.userId
-                }
-            },
-            order: {
-                date: 'DESC',
-                timestamp: 'DESC'
-            }
-        };
+        let builder = this.flightRepository.createQueryBuilder('flight')
+            .addSelect('user')
+            .leftJoin('flight.user', 'user', 'user.id = flight.user_id')
+            .leftJoinAndSelect('flight.glider', 'glider', 'glider.id = flight.glider_id')
+            .leftJoinAndSelect('flight.start', 'start', 'start.id = flight.start_id')
+            .leftJoinAndSelect('flight.landing', 'landing', 'landing.id = flight.landing_id')
+            .where(`user.id = ${token.userId}`)
 
         if (query && query.limit) {
             if (Number.isNaN(Number(query.limit))) {
                 throw new BadRequestException("limit is not a number");
             };
-            options.take = query.limit;
+            builder.take(query.limit)
         }
 
         if (query && query.offset) {
             if (Number.isNaN(Number(query.offset))) {
                 throw new BadRequestException("offset is not a number");
             };
-            options.skip = query.offset;
+            builder.skip(query.offset);
         }
 
-        return await this.flightRepository.find(options);
+        if (query && query.glider) {
+            if (Number.isNaN(Number(query.glider))) {
+                throw new BadRequestException("glider is not a number");
+            };
+            builder.andWhere(`glider.id = ${query.glider}`);
+        }
+
+        if (query && query["glider-type"]) {
+            if (Number.isNaN(Number(query["glider-type"]))) {
+                throw new BadRequestException("type is not a 0 or 1");
+            };
+            builder.andWhere(`glider.tandem = ${query["glider-type"]}`)
+        }
+
+        if (query && query.from) {
+            builder.andWhere(`flight.date >= "${query.from}"`);
+        }
+
+        if (query && query.to) {
+            builder.andWhere(`flight.date <= "${query.to}"`);
+        }
+
+        builder.orderBy('flight.date', 'DESC');
+        builder.addOrderBy('flight.timestamp', 'DESC');
+
+        return builder.getMany();
     }
 
     async saveFlight(flight: Flight): Promise<Flight | undefined> {
@@ -51,6 +71,6 @@ export class FlightService {
     }
 
     async getFlightById(token: any, id: number): Promise<Flight> {
-        return this.flightRepository.findOneOrFail({id: id, user: {id: token.userId }});
+        return this.flightRepository.findOneOrFail({ id: id, user: { id: token.userId } });
     }
 }
