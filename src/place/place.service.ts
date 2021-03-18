3563dc0bfd847eeb3f00f6a34b2e7ed28b3da026
fabiangotzen <fabian.gotzen@gmail.com>
2021-03-18 19:00:26 +0100

    fix typo

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, ILike } from 'typeorm';
+import { Repository, Like } from 'typeorm';
 import { Place } from './place.entity';
 import { PagerDto } from 'src/interface/pager-dto';
 
@@ -97,7 +97,7 @@ export class PlaceService {
                 user: {
                     id: token.userId
                 },
-                name: ILike("%" + name + "%")
+                name: Like(`%${name}%`)
             },
             order: {
                 name: 'ASC'
