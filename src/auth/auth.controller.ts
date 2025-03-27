6a26b91712f3be325a502075d57b6efd91d7c6ca
Yannick Lagger <lagger.yannick@gmail.com>
2025-03-27 22:53:41 +0100

    Add post logout and refresh endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { LocalAuthGuard } from './guard/local-auth.guard';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
 import { AuthFacade } from './auth.facade';
 import { LoginDto } from './interface/login-dto';
-import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 @ApiTags('Auth')
@@ -22,17 +22,34 @@ export class AuthController {
         return this.authFacade.googleLogin(token, language);
     }
 
+    @ApiOperation({ deprecated: true })
     @Get('refresh/:token')
     @ApiParam({name: 'token', required: true, schema: { oneOf: [{type: 'string'}]}})
     async refresh(@Param('token') token, @Headers('accept-language') language: string) {
         return this.authFacade.refresh(token, language);
     }
 
+    @Post('refresh')
+    @ApiParam({name: 'refresh_token', required: true, schema: { oneOf: [{type: 'string'}]}})
+    async refreshPost(@Body('refresh_token') refreshToken: string, @Headers('accept-language') language: string) {
+        return this.authFacade.refresh(refreshToken, language);
+    }
+
+    @ApiOperation({ deprecated: true })
     @UseGuards(JwtAuthGuard)
     @Get('logout')
     @HttpCode(204)
     @ApiBearerAuth('jwt')
     async logout(@Request() req) {
+        await this.authFacade.logout(req.user)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('logout')
+    @HttpCode(204)
+    @ApiBearerAuth('jwt')
+    @ApiParam({name: 'refresh_token', required: true, schema: { oneOf: [{type: 'string'}]}})
+    async logoutPost(@Request() req, @Body('refresh_token') refreshToken: string) {
         await this.authFacade.logout(req.user)
     }
 
