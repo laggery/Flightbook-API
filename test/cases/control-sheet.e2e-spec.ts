8fd07d6d002c0c7aa71b35bad71264c9f4f90459
Yannick Lagger <lagger.yannick@gmail.com>
2025-11-22 21:05:06 +0100

    Add instructor control-sheet e2e test

--- test/cases/control-sheet.e2e-spec.ts
+++ test/cases/control-sheet.e2e-spec.ts
@@ -56,4 +56,53 @@ describe('Student ControlSheet (e2e)', () => {
 });
 
 describe('Instructor ControlSheet (e2e)', () => {
+  const testInstance = new BaseE2ETest();
+
+  beforeEach(async () => {
+    await testInstance.cleanupBetweenTests();
+  });
+
+  it('/instructor/students/:id/control-sheet (GET)', async() => {
+    // given
+    const { student, instructorUser } = await testInstance.createSchoolData();
+    const controlSheet = Testdata.createControlSheet(true);
+    await testInstance.controlSheetRepository.save(controlSheet);
+    
+    const keycloakToken = JwtTestHelper.createKeycloakToken({ sub: instructorUser.id, email: instructorUser.email });
+
+    //when
+    return request(testInstance.app.getHttpServer())
+      .get(`/instructor/students/${student.id}/control-sheet`)
+      .set('Authorization', `Bearer ${keycloakToken}`)
+      .expect(200)
+      .then(response => {
+        expect(removeIds(response.body)).toMatchSnapshot();
+      });
+  });
+
+  it('/instructor/student/:id/control-sheet (POST)', async() => {
+    // given
+    const { student, instructorUser } = await testInstance.createSchoolData();
+    const controlSheet = new ControlSheet();
+    controlSheet.trainingHill.aufziehen = 2;
+    const keycloakToken = JwtTestHelper.createKeycloakToken({ sub: instructorUser.id, email: instructorUser.email });
+
+    //when
+    return request(testInstance.app.getHttpServer())
+      .post(`/instructor/students/${student.id}/control-sheet`)
+      .set('Authorization', `Bearer ${keycloakToken}`)
+      .send(controlSheet)
+      .expect(201)
+      .then(async (response) => {
+        expect(removeIds(response.body)).toMatchSnapshot();
+        expect(response.body.id).toBeDefined();
+
+        const db = await testInstance.controlSheetRepository.findOne({
+            where: { id: response.body.id },
+            relations: ['trainingHill', 'altitudeFlight', 'theory', 'level', 'user']
+        });
+        expect(removeIds(db)).toMatchSnapshot();
+        expect(db.id).toEqual(response.body.id);
+      });
+  });
 });
